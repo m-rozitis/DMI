980a40212bb8fd9e1396bf664816eaa3ed546e1e
User <rozitismartins@gmail.com>
2018-01-25 23:33:01 +0200

    20180125

--- LD_0.sh
+++ LD_0.sh
@@ -2,13 +2,28 @@
 echo "Lietotāj, lūdzu, ievadi skaitļus: "
 read input
 a=($input)
-
 n=${#a[@]}
 
+
 sort=($(printf "%s\n" "${a[@]}" | sort -n))
-echo ${sort[@]}
-echo "Lielākais skaitlis ir " ${sort[0]}
-echo "Mazākais skaitlis ir " ${sort[n-1]}
+echo "Skaitļi augošā secībā: "${sort[@]}
+
+
+echo "Lielākais skaitlis ir " ${sort[n-1]}
+echo "Mazākais skaitlis ir " ${sort[0]}
+
+
+summa=0
+k=0
+while (( $k < $n ))
+do
+	summa=`expr $summa + ${sort[$k]}`
+	k=`expr $k + 1`
+done
+vsk=`expr $summa / $n`
+dsk=`expr $summa % 1$n`
+echo "Vidējā vērtība ir " $vsk "." $dsk
+
 
 if (( n % 2 == 1 ))
 then
@@ -16,36 +31,31 @@ then
 	echo "${sort[($n-1)/2]}" | bc
 else
 	echo -n "Mediāna ir "
-	echo "scale=1;(${sort[$n/2]} + ${sort[$n/2+1]})/2" |bc
+	echo "scale=2;(${sort[$n/2]} + ${sort[$n/2-1]})/2" |bc
 fi
 
-t=0
-a1=${sort[$t]}
-a2=1
-b1=$a1
-b2=$a2
 
-while (( $t < $n ))
+k=0
+sk1=${sort[$k]}
+n1=1
+sk2=$sk1
+n2=$n1
+
+while (( $k < $n ))
 do
-	t=`expr $t + 1`
-	if [[ "$a1" == "${sort[t]}" ]]
+	k=`expr $k + 1`
+	if [[ "$sk1" == "${sort[k]}" ]]
 	then
-		a2=`expr $a2 + 1`
+		n1=`expr $n1 + 1`
 	else
-		a1=${sort[$t]}
-		a2=1
+		sk1=${sort[$k]}
+		n1=1
 	fi
 
-	if (( $a2 > $b2))
+	if (( $n1 > $n2))
 	then
-		b1=$a1
-		b2=$a2
+		sk2=$sk1
+		n2=$n1
 	fi
 done
-
-if (( $b2 == 1 ))
-then
-	echo "Visi skaitli atkartojas vienu reizi"
-else
-	echo "Moda:" $b1
-fi
+echo "Moda:" $sk2
